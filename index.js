64a6bb7c7b8b21941b7de25902ca47bfbfdfca2d
IBasavaraj <105215381+IBasavaraj@users.noreply.github.com>
2022-05-10 18:02:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const mobNumForm = document.getElementById("mblNumForm");
+const otp = document.getElementById("otp");
+// mblNumForm.addEventListener("submit", () => {
+//     const _data = mblNumForm.elements["mobileNumber"].value;
+
+//   fetch('https://nydk62oop8.execute-api.ap-south-1.amazonaws.com/dev/login', {
+//   method: "POST",
+//   body: JSON.stringify(_data),
+//   headers: {"Content-type": "application/json; charset=UTF-8"}
+// })
+// .then(response => response.json()) 
+// .then(json => console.log(json))
+// .catch(err => console.log(err))
+// })
+
+// otp.addEventListener("submit", () => {
+//     alert("shgh");
+//     const otpNum = otp.elements["otp"];
+//     // alert(otpNum)
+//     fetch('https://nydk62oop8.execute-api.ap-south-1.amazonaws.com/dev/verifyOTP', {
+//   method: "POST",
+//   body: JSON.stringify(otpNum),
+//   headers: {"Content-type": "application/json; charset=UTF-8"}
+// })
+// .then(response => response.json()) 
+// .then(json => console.log(json))
+// .catch(err => console.log(err))
+
+// })
